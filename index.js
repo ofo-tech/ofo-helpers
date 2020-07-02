337f5c419da8925ee4c0ecf91089e2bd202720be
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2020-07-02 15:46:18 +0800

    bug fix

--- index.js
+++ index.js
@@ -38,3 +38,17 @@ export const reorder_rectangle_points = points => {
   }
   return output;
 }
+
+export const toTitle = str => !str ? '' : str.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ');
+
+export const isDefined = i => typeof i != 'undefined' && i != null;
+
+export const isValidURL = str => {
+  var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  return !!pattern.test(str);
+}
